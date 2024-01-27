edccb2a4735ca6cd79c017f55020b0388935a0f1
Qinan Gao <qinanagao@gmail.com>
2024-01-27 22:19:37 +0000

    loop the data

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,16 @@
 import AccordionItem from "./AccordionItem";
 
-export default function Accordion() {
-  return <div>TODO</div>;
+export default function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, index) => (
+        <AccordionItem
+          title={el.title}
+          text={el.text}
+          num={index}
+          key={index}
+        />
+      ))}
+    </div>
+  );
 }
