e374f480e1678aff5316df15612db5cf11cd7c23
Qinan Gao <qinanagao@gmail.com>
2024-01-27 22:19:48 +0000

    add usestate

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,1 +1,16 @@
-export default function AccordionItem() {}
+import { useState } from "react";
+
+export default function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <div className={isOpen ? "item open" : "item"} onClick={handleClick}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className={"title"}>{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen ? <div className="content-box">{text}</div> : null}
+    </div>
+  );
+}
