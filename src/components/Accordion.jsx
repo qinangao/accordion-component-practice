7743a217aef91e92e5dd15158d967e7060449039
Qinan Gao <qinanagao@gmail.com>
2024-01-29 21:19:05 +0000

    lift uo state

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,36 @@
 import AccordionItem from "./AccordionItem";
+import { useState } from "react";
 
 export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, index) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           title={el.title}
-          text={el.text}
           num={index}
           key={index}
-        />
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Section 4"
+        num="4"
+        key="04"
+      >
+        <p>test test test </p>
+        <ul>
+          <li>dsafasdf</li>
+          <li>dfasdfasd</li>
+          <li>dsfsadfsad</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
