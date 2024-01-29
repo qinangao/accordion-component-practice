598677277066dda24603685a3c1b34ab2e243107
Qinan Gao <qinanagao@gmail.com>
2024-01-29 21:19:17 +0000

    add children props

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+export default function AccordionItem({
+  num,
+  title,
+  curOpen,
+  onOpen,
+  children,
+}) {
+  const isOpen = num === curOpen;
 
-export default function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
   function handleClick() {
-    setIsOpen(!isOpen);
+    // setIsOpen(!isOpen);
+    onOpen(isOpen ? null : num);
   }
   return (
     <div className={isOpen ? "item open" : "item"} onClick={handleClick}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className={"title"}>{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen ? <div className="content-box">{text}</div> : null}
+      {isOpen ? <div className="content-box">{children}</div> : null}
     </div>
   );
 }
